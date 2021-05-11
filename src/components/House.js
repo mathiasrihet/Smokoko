00172e68c828e4db6c8a59299d24beab3d81e619
Mathias Rihet <73931792+mathiasrihet@users.noreply.github.com>
2021-05-11 17:49:19 +0200

    creation bouton

--- src/components/House.js
+++ src/components/House.js
@@ -29,15 +29,36 @@ export default class House extends React.Component {
                     width: 586px;
                     z-index: 0;
                 }
+                .button-container {
+                    display: flex;
+                    margin: 10px;
+                    padding: 10px;
+                }
+                .button {
+                    margin: 10px;
+                }
           `}
                 </style>
-                <div className="level-container">
+            <div>
+                <div>
                     <Pet />
                     <div className="pet-area">
                         <img className="pet-area-img" src={gameBackground}/>
                     </div>
                 </div>
+                <div className="button-container">
+                    <div className="button">
+                        <button>Jouer</button>
+                    </div>
+                    <div className="button">
+                        <button>Dormir</button>
+                    </div>
+                    <div className="button">
+                        <button>Manger</button>
+                    </div>
+                </div>
             </div>
+    </div>
         )
     }
 }
