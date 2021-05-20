4500fe8e19f51095c84d6de97759e6da3c69d486
Mathias Rihet <73931792+mathiasrihet@users.noreply.github.com>
2021-05-20 14:38:02 +0200

    changement de repository

---
+++ api_repository/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '035dcfa5cd059c8494bed1380b63de98'),
+    },
+  },
+});
---
+++ api_repository/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bbecf9b0-9637-4eda-afda-8fa5635f75bd'
+};
